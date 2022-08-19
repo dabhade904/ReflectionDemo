ca4bfa2aa282e9bb78f24bd41727091f1a53313d
dabhade904 <dabhade904@gmail.com>
2022-08-19 15:59:46 +0530

    Added : Reflection demo project

---
+++ Add.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionDemo
+{
+    public class Add
+    {
+        public Add() { }
+        public Add(int a) { }
+        public Add(int a,int b) { }
+        public Add(string a) { } 
+
+        public void m1()
+        {
+
+        }
+
+        public static void m2() { }
+        private static void m3() { }
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using ReflectionDemo;
+using System.Reflection;
+
+namespace Reflection
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a = 10;
+            Type t = a.GetType();
+            Console.WriteLine(t);
+            Type type = typeof(System.String);
+            Console.WriteLine(type);
+            Console.WriteLine(type.Assembly);
+            Console.WriteLine("-------------------------------");
+            Type type1 = typeof(Add);
+            ConstructorInfo[] p = type1.GetConstructors(
+             BindingFlags.Public | BindingFlags.Static |
+             BindingFlags.NonPublic | BindingFlags.Instance);
+            foreach (ConstructorInfo c in p)
+            {
+                Console.WriteLine(c);
+            }
+            Console.WriteLine("------------------------------");
+            MethodInfo[] m = type1.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static);
+            foreach (MethodInfo m2 in m)
+            {
+                Console.WriteLine(m2);
+            }
+        }
+    }
+}
